c26cadedef2d6b7da9e3f6d52d0a613e27a97471
Tiago Martinho <t.martinho@icloud.com>
2018-10-20 11:07:29 +0200

    Initial Commit

---
+++ ReCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReCounter
+//
+//  Created by Tiago Martinho on 20/10/2018.
+//  Copyright © 2018 tm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
